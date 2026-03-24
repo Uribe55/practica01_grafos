6102cbba1299e2e300bd22e5f497c8c82e906eb4
Uribe <Uribe@DESKTOP-9P6DTM6>
2026-03-23 21:15:15 -0400

    Registro de nombre y desarrollo de la clase Grafo

--- Grafo.java
+++ Grafo.java
@@ -1,3 +1,64 @@
-public class Grafo {
+import java.util.Arrays;
 
+public class Grafo {
+private int vertices;
+    private int arcos;
+    private Lista<Integer>[] enlaces;
+
+    public Grafo(int v){
+        vertices = v;
+        arcos = 0;
+        enlaces = new Lista[v];
+
+        for(int i = 0; i < v; i++){
+            enlaces[i] = new Lista<>();
+        }
+    }
+
+    public int getVertices() {
+        return vertices;
+    }
+    
+    public void conectar(int v, int w){
+        arcos++;
+        enlaces[v].insertar(w);
+        enlaces[w].insertar(v);
+    }
+
+    public Iterable<Integer> getConexiones(int n){
+        return enlaces[n];
+    }
+
+    public boolean conectado(int v, int w){
+        Pila<Integer> frontera = new Pila<>();
+        frontera.empilar(v);
+        Lista<Integer> vistados = new Lista<>();
+        Integer nodo;
+        while (!frontera.esta_vacio()) {
+            nodo = frontera.desempilar();
+            if (nodo == w){
+                return true;
+            }
+            vistados.insertar(nodo);
+            for (Integer i : enlaces[nodo]) {
+                if(!vistados.pertenece(i))
+                frontera.empilar(i);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(vertices + " vertices " + arcos + " enalces " + "\n");
+        for(int i = 0; i < vertices; i++){
+            s.append(i + " : ");
+            for (int j : enlaces[i]) {
+                s.append(j + " ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # practica01_grafos
 Actividad Pr´actica: Optimizaci´on de Rutas de Delivery en Cochabamba
+Integrantes:
+Ona Alcoba Sebastian Alexandre
+Perez Torrico Sebastian
+Uribe Pessoa Eric
